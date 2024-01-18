d4c0d8f6ec58dd8433c6ac35b9c29d1958b5a269
Lucas Flores <flores.lucas@gmail.com>
2024-01-17 21:22:00 -0300

    Criado classe epsódio e melhorado classe princpal com LocalDate

--- src/main/java/br/com/flores/screenmetch/principal/Principal.java
+++ src/main/java/br/com/flores/screenmetch/principal/Principal.java
@@ -3,12 +3,17 @@ package br.com.flores.screenmetch.principal;
 import br.com.flores.screenmetch.model.DadosEpsodio;
 import br.com.flores.screenmetch.model.DadosSerie;
 import br.com.flores.screenmetch.model.DadosTemporada;
+import br.com.flores.screenmetch.model.Epsodio;
 import br.com.flores.screenmetch.service.ConsumoApi;
 import br.com.flores.screenmetch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -35,6 +40,40 @@ public class Principal {
 
         temporadas.forEach(System.out::println);
         temporadas.forEach(t -> t.epsodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpsodio> dadosEpsodios = temporadas.stream()
+                .flatMap(t -> t.epsodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 epsódios");
+        dadosEpsodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpsodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Epsodio> epsodios = temporadas.stream()
+                .flatMap(t -> t.epsodios().stream()
+                        .map(d -> new Epsodio(t.numero(),d ))
+                ).collect(Collectors.toList());
+
+        epsodios.forEach(System.out::println);
+
+        System.out.println("A partir de qual ano você deseja ver os epsódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyy");
+
+        epsodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Epsódio: " + e.getTitulo() +
+                                " Data Lançamento: " +e.getDataLancamento().format(formatador)
+                ));
     }
 
 }
